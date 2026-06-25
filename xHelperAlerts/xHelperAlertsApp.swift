b54e1ab75c5f391c187c2040794204e4561b1e08
Sarah Holder <sarahh@auctionedge.com>
2026-06-25 18:51:14 -0500

    Initial Commit

---
+++ xHelperAlerts/xHelperAlertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  xHelperAlertsApp.swift
+//  xHelperAlerts
+//
+//  Created by Sarah Holder on 2026/6/25.
+//
+
+import SwiftUI
+
+@main
+struct xHelperAlertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
